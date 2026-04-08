d99d983779533896a0ec9a51450a7c66ba3b4298
MEGAMIND <x@MEGAMINDs-MacBook-Air.local>
2026-04-08 03:09:45 +0300

    fix host port

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+import os
+
 from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 import re
@@ -79,4 +82,4 @@ def remove_patient(pid):
     return redirect(url_for("patient_list"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
